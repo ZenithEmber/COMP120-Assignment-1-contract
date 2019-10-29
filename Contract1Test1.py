ec6d7cd07bdf8b252c470bdae42ada39ef2a74ab
ZenithEmberq <ph230156@falmouth.ac.uk>
2019-10-29 18:21:00 +0000

    Added the contract program to the repo, I have also commented on a few lines of code within the program.

---
+++ Contract1Test1.py
@@ -0,0 +1,42 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720)) # this line of code creates a display window which lets the user see what the program is doing.
+pygame.display.set_caption("Contract 1 Tile Generator")
+clock = pygame.time.Clock()
+
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+RED = 255, 0, 0
+
+Tiles = ("grass", "stone", "sand") #this is the list of tiles which the user may input to get a solid colour box.
+
+Grass_color = 0, 204, 0
+Stone_color = 128, 128, 128
+Sand_color = 218, 183, 12
+#these values determine the colour of the tile, it is RGB(Red, Green, Blue). There is a max value of 255 in each colour or in hexadecimal, it is known as FF.
+
+tile = str(input("Enter which tile you would like, selection is: Grass, Stone, Sand: ")) #this line of code is waiting for the user to input a string, while it is waiting it prints out the message between the speech marks.
+
+tile.lower
+
+running = True
+while running: #this is a basic iteration
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # this line of code determines whether the user has clicked the exit button in the top right, without this code the user wouldn't be able to close the program.
+            running = False
+            screen.fill(BLACK)
+
+        if tile == "sand":
+            pygame.draw.rect(screen, Sand_color, (10, 10, 50, 50))
+        elif tile == "stone":
+            pygame.draw.rect(screen, Stone_color, (10, 10, 50, 50))
+        elif tile == "grass":
+            pygame.draw.rect(screen, Grass_color, (10, 10, 50, 50))
+        else:
+            pygame.draw.rect(screen, WHITE, (10, 10, 50, 50)) #these numbers in brackets add a padding and set the size of the box. As well as the WHITE colour, the others call the RGB values which are assigned to each different tile.
+
+        pygame.display.flip()
+
+sys.exit() #this line of code exits the program when the user wants to stop using the program.
---
+++ README.txt
@@ -0,0 +1,10 @@
+
+# # # Tinkering graphics assignment: Contract 1 # # #
+
+This is conract 1, to use this program all you do is open it using PyCharm and hit the run button(or F5 key). 
+To get the tiles type in grass, sand or stone and the box will appear to match the colour of the specified input.
+
+We aim to improve the code drasticly in future by adding much more varied tiles aswell as the ability
+to input multiple with different attributes.
+
+This was coded by both Toby Atkinson and Philip Hutchings, we both took in turns doing the programming.
