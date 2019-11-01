cc529eaaa9aaf3a841ff58586e38392fb869eddc
ZenithEmberq <ph230156@falmouth.ac.uk>
2019-11-01 08:55:44 +0000

    edited code

--- Contract1Test1.py
+++ Contract1Test1.py
@@ -10,7 +10,7 @@ BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 255, 0, 0
 
-Tiles = ("grass", "stone", "sand") #this is the list of tiles which the user may input to get a solid colour box.
+Tiles = ("grass", "stone", "sand", "Grass", "Stone", "Sand") #this is the list of tiles which the user may input to get a solid colour box.
 
 Grass_color = 0, 204, 0
 Stone_color = 128, 128, 128
@@ -21,22 +21,22 @@ tile = str(input("Enter which tile you would like, selection is: Grass, Stone, S
 
 tile.lower
 
-running = True
-while running: #this is a basic iteration
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: # this line of code determines whether the user has clicked the exit button in the top right, without this code the user wouldn't be able to close the program.
-            running = False
-            screen.fill(BLACK)
+if tile in Tiles:
+	while running: #this is a basic iteration
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: # this line of code determines whether the user has clicked the exit button in the top right, without this code the user wouldn't be able to close the program.
+				running = False
+				screen.fill(BLACK)
 
-        if tile == "sand":
-            pygame.draw.rect(screen, Sand_color, (10, 10, 50, 50))
-        elif tile == "stone":
-            pygame.draw.rect(screen, Stone_color, (10, 10, 50, 50))
-        elif tile == "grass":
-            pygame.draw.rect(screen, Grass_color, (10, 10, 50, 50))
-        else:
-            pygame.draw.rect(screen, WHITE, (10, 10, 50, 50)) #these numbers in brackets add a padding and set the size of the box. As well as the WHITE colour, the others call the RGB values which are assigned to each different tile.
+			if tile == "sand":
+				pygame.draw.rect(screen, Sand_color, (10, 10, 50, 50))
+			elif tile == "stone":
+				pygame.draw.rect(screen, Stone_color, (10, 10, 50, 50))
+			elif tile == "grass":
+				pygame.draw.rect(screen, Grass_color, (10, 10, 50, 50))
+			else:
+				pygame.draw.rect(screen, WHITE, (10, 10, 50, 50)) #these numbers in brackets add a padding and set the size of the box. As well as the WHITE colour, the others call the RGB values which are assigned to each different tile.
 
-        pygame.display.flip()
-
-sys.exit() #this line of code exits the program when the user wants to stop using the program.
+			pygame.display.flip()
+	
+	sys.exit() #this line of code exits the program when the user wants to stop using the program.
